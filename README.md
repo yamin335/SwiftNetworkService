1eba96c335a04ca3c22375ce6122d386c8829626
Yamin <myamin335@gmail.com>
2023-07-02 17:23:26 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftNetworkService
+
+A description of this package.
---
+++ Tests/SwiftNetworkServiceTests/SwiftNetworkServiceTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftNetworkService
+
+final class SwiftNetworkServiceTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftNetworkService().text, "Hello, World!")
+    }
+}
